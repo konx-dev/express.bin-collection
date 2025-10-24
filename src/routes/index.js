dc98a19c77dff0510361a7116f5e5cfd1342ff92
Oliver Cox <github@konx.dev>
2025-10-24 11:54:11 +0100

    adding test code

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,4 +12,21 @@ router.get('/', (req, res) => {
     });
 });
 
+
+/**
+ * DANGEROUS TEST ROUTE - DO NOT USE IN PRODUCTION
+ * This route contains a vulnerable regular expression for testing CodeQL.
+ */
+router.get('/test-redos/:input', (req, res) => {
+    const { input } = req.params;
+    const vulnerableRegex = /^(a+)+$/;
+
+    // This line will cause the server to hang with a malicious input
+    if (vulnerableRegex.test(input)) {
+        res.send('Input matched (vulnerable).');
+    } else {
+        res.send('Input did not match.');
+    }
+});
+
 module.exports = router;
